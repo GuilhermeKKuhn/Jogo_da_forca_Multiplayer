4ccbd16a4881b93f9a025afd145a79bd6745f8f9
Guilherme Kuhn <guilhermekurpe@gmail.com>
2023-09-20 19:43:35 -0300

    teste

---
+++ src/main/java/org/example/Player.java
@@ -0,0 +1,53 @@
+package org.example;
+
+public class Player {
+    private String nome;
+    private String ip;
+
+    private int vidas;
+
+    private double pontos;
+
+    public Player(String nome, String ip) {
+        this.nome = nome;
+        this.ip = ip;
+        this.vidas = 5;
+        this.pontos = 0;
+    }
+
+    public Player() {
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getIp() {
+        return ip;
+    }
+
+    public void setIp(String ip) {
+        this.ip = ip;
+    }
+
+    public int getVidas() {
+        return vidas;
+    }
+
+    public void setVidas(int vidas) {
+        this.vidas = vidas;
+    }
+
+    public double getPontos() {
+        return pontos;
+    }
+
+    public void setPontos(double pontos) {
+        this.pontos = pontos;
+    }
+
+}
