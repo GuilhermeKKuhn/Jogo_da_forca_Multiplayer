9e1002c94f04e0c70dab8d2917c790fd437c5abc
Lucas Bertella <132720460+LucasBertella1@users.noreply.github.com>
2023-09-20 21:05:41 -0300

    login

---
+++ src/main/java/org/example/View/TelaPrincipal.java
@@ -0,0 +1,9 @@
+package org.example.View;
+
+import javax.swing.*;
+
+public class TelaPrincipal {
+    private JTextField textField1;
+    private JTextField textField2;
+    private JButton iniciarButton;
+}
